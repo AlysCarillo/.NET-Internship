ef3cc147929e4b571c36e7cd6c6e3184fe1affb7
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-05-18 22:39:42 +0800

    Replace BinaryTree with TreeStructure and create CalculateDepth method

---
+++ TreeDepthCalculator/TreeStructure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TreeDepthCalculator.Models;
+
+namespace TreeDepthCalculator
+{
+    public static class TreeStructure
+    {
+        public static int CalculateDepth(this Branch branch)
+        {
+            if (branch.Branches.Count == 0)
+            {
+                return 1;
+            }
+
+            int maxDepth = 0;
+            foreach (var child in branch.Branches)
+            {
+                int depth = child.CalculateDepth();
+                if (depth > maxDepth)
+                {
+                    maxDepth = depth;
+                }
+            }
+
+            return maxDepth + 1;
+        }
+    }
+}
