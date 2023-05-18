c3d00af1f042a4a56e07a937ba5d0a1c5d0d7ccf
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-05-18 22:39:02 +0800

    Replace Node with Branch

---
+++ TreeDepthCalculator/Models/Branch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDepthCalculator.Models
+{
+    public class Branch
+    {
+        public string Name { get; set; }
+        public List<Branch> Branches { get; set; }
+
+        public Branch(string name)
+        {
+            Name = name;
+            Branches = new List<Branch>();
+        }
+
+        public override string ToString()
+        {
+            return ToStringHelper(this, "");
+        }
+
+        private string ToStringHelper(Branch branch, string indent)
+        {
+            string result = $"{indent}{branch.Name}\n";
+            string childIndent = indent + "   ";
+
+            foreach (var child in branch.Branches)
+            {
+                result += ToStringHelper(child, childIndent);
+            }
+
+            return result;
+        }
+    }
+}
