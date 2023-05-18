e03680cb37345bec453a0761feb5834ddde1358f
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-05-18 21:51:35 +0800

    Create AddNode method

---
+++ TreeDepthCalculator/BinaryTree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeDepthCalculator.Models;
+
+namespace TreeDepthCalculator
+{
+    public class BinaryTree
+    {
+        private Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public void AddNode(int value)
+        {
+            root = RecursiveAddNode(root, value);
+        }
+
+        private Node RecursiveAddNode(Node current, int value)
+        {
+            if (current == null)
+            {
+                Node newNode = new Node();
+                newNode.Value = value;
+                return newNode;
+            }
+
+            if (value < current.Value)
+            {
+                current.Left = RecursiveAddNode(current.Left, value);
+            }
+            else if (value > current.Value)
+            {
+                current.Right = RecursiveAddNode(current.Right, value);
+            }
+
+            return current;
+        }
+
+        
+    }
+}
